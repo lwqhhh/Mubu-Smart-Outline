62bca06f0b7596b133bfab7a02f74aff3bf90fcc
lwqhhh <82591085+lwqhhh@users.noreply.github.com>
2026-01-27 15:21:10 +0800

    Update OutlineScript.user.js

--- OutlineScript.user.js
+++ OutlineScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         幕布目录_V1.0.0
+// @name         幕布目录
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  新增URL监听，切换文档时自动清空旧大纲，修复内容重叠问题
 // @author       Vachel
 // @match        https://mubu.com/*
